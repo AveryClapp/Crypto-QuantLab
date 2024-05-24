397435520514b5fe7459c2f17728f9858488fdf3
Avery Clapp <averyclapp@Averys-MacBook-Pro-2.local>
2024-05-23 22:08:13 -0400

    Establishing preliminary project structure:

---
+++ scoring_algo.py
@@ -0,0 +1,7 @@
+#Handles how the scoring algorithm works and the parameters for the AI that go into the calculations
+
+'''
+First Thoughts:
+Key Phrases (good and bad)
+Time since news article (the more recent the better)
+'''
