2fa53aca1f89cd1d21dd1ff1151f3a4be6ccad0b
AveryClapp <avery.clapp@gmail.com>
2024-06-26 22:27:20 -0400

    fixing problems with boto3 connection

--- app/data/get_financial_data.py
+++ app/data/get_financial_data.py
@@ -2,32 +2,58 @@ import requests
 import os
 import os.path
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
-from bs4 import BeautifulSoup 
+from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 import pandas as pd
-import jsond
+import json
 from datetime import datetime as dt
+import boto3
+import io
 
 # Market trends can look at dominance at https://www.coingecko.com/en/coins/{crypto} <- crypto symbol not necessarily required.
 
-def main(crypto):    
-    path = f'./app/data/csvs/{crypto}_financials.csv'
-    if not os.path.exists(path):
-        with open(path, "w") as file:
-            file.write("Time,Price,Daily Volume,Daily Volume Change,Market Cap,1D Delta,7D Delta,F&G,BTC Dominance, Stablecoin Volume, Total Market Cap \n")
-        print(f'{path} file created')
-    data = pd.read_csv(path)
+def main(crypto):
+    load_dotenv('app/core/.env')
+    aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+    aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+    s3 = boto3.client('s3',
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        region_name='us-east-2')
+    bucket_name='cryptopltfdatabucket'
+    file = f'{crypto}_financials.csv'
+    try:
+        response = s3.get_object(Bucket=bucket_name, Key=file)
+        print(response)
+        data = pd.read_csv(io.BytesI0(response['Body'].read()))
+    except Exception as e:
+        print(e)
+        data = pd.DataFrame(columns=["Time","Price","Daily Volume","Daily Volume Change","Market Cap",
+            "1D Delta","7D Delta","F&G","BTC Dominance","Stablecoin Volume","Total Market Cap"])
     time = dt.utcnow().strftime("%m-%d-%Y %H:%M:%S")
     market_cap, price, daily_change, weekly_change, daily_volume, daily_volume_change, btc_dominance, stablecoin_volume, total_mc = coinmarketcap_data(crypto)
     fear_and_greed = market_trends(crypto)
-    # Add data to the end of the dataframe
-    data.loc[len(data)] = [time, round(price,2), round(daily_volume,2), round(daily_volume_change,2), round(market_cap,2), 
-        round(daily_change,2), round(weekly_change,2), fear_and_greed, round(btc_dominance,2), round(stablecoin_volume,2),round(total_mc,2)]
-    data.to_csv(path, index=False)
+    data.loc[len(data)] = [
+        time,
+        round(price, 2),
+        round(daily_volume, 2),
+        round(daily_volume_change, 2),
+        round(market_cap, 2),
+        round(daily_change, 2),
+        round(weekly_change, 2),
+        fear_and_greed,
+        round(btc_dominance, 2),
+        round(stablecoin_volume, 2),
+        round(total_mc, 2)
+    ]
+    csv_buffer = io.StringIO()
+    data.to_csv(csv_buffer, index=False)
+    s3.put_object(Bucket=bucket_name, Key=file, Body=csv_buffer.getvalue())
+    print(f"Success at time: {time}")
     return 'Success'
 
 def coinmarketcap_data(crypto):
-    load_dotenv('./app/core/.env')
+    load_dotenv('/home/ec2-user/data/.env')
     url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
     url2 = 'https://pro-api.coinmarketcap.com/v1/global-metrics/quotes/latest'
     api = os.getenv('CMC_KEY')
@@ -59,7 +85,7 @@ def coinmarketcap_data(crypto):
         print(f"Error occurred during second CMC API request: {e}")
         raise RuntimeException("Error occurred during second CMC API request")
     btc_dominance = data['data']['btc_dominance']
-    stablecoin_volume = data['data']['quote']['USD']['stablecoin_volume_24h']  
+    stablecoin_volume = data['data']['quote']['USD']['stablecoin_volume_24h']
     total_market_cap = data['data']['quote']['USD']['total_market_cap']
     return market_cap, price, daily_change, weekly_change, daily_volume, daily_volume_change, \
         btc_dominance, stablecoin_volume, total_market_cap
@@ -73,4 +99,3 @@ def market_trends(crypto):
 
 if __name__ == "__main__":
     main("bitcoin")
-
